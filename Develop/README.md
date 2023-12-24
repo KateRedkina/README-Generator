718b7f5dbbbef644d78cc363e552f89072154ce3
Kateryna Redkina <redkinakate@gmail.com>
2023-12-23 20:54:28 -0500

    add code for readme generator

---
+++ Develop/README.md
@@ -0,0 +1,39 @@
+ # **_Test_**
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  ## **_Description_**
+  Test
+
+  ## **_Table of contents_**
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [License](#license)
+  * [Questions](#questions)
+    
+  ## **_Installation_**
+  Test
+
+  ## **_Usage_**
+  Test
+
+  ## **_Contributing_**
+  Test
+
+  ## **_Tests_**
+  Test
+
+## **_License_**
+    
+This project is covered under the MIT license.
+Click [here](
+        https://opensource.org/licenses/MIT
+        ) to access more information on the license.
+
+  ## **_Questions_**
+  Don't hesitate to visit [my GitHub](https://github.com/Test)
+  or reach me on my [email address](mailto:Test) for additional questions.
+  
---
+++ Develop/index.js
@@ -0,0 +1,139 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+// TODO: Create an array of questions for user input
+const questions = portfolioData => {
+    return inquirer.prompt([
+    {
+     type: 'input',
+     name: 'projectTitle',
+     message: 'What is the name of your project? (Required)',
+     validate : 
+        projectTitleInput => {
+            if (projectTitleInput) {
+                return true;
+            } else {
+                console.log('You need to enter a project name!');
+                return false;
+            }
+        }  
+    },
+    {
+     type: 'input',
+     name: 'description',
+     message: 'Provide a description of the project (Required)',
+     validate : 
+        descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('You need to enter a project description!');
+                return false;
+            }
+        }  
+    },
+    {
+     type: 'input',
+     name: 'installation',
+     message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running. (Required)',
+     validate : 
+        installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('You need to enter installation information!');
+                return false;
+            }
+        }  
+    },
+    {
+     type: 'input',
+     name: 'usage',
+     message: 'Provide instructions and examples for use. (Required)',
+     validate : 
+        usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('You need to enter usage information!');
+                return false;
+            }
+        }  
+    },
+    {
+     type: 'input',
+     name: 'contributing',
+     message: 'Provide contributors of the project. (Required)',
+     validate : 
+        contributingInput => {
+            if (contributingInput) {
+                return true;
+            } else {
+                console.log('You need to enter contribution information!');
+                return false;
+            }
+        }  
+    },
+    {
+     type: 'input',
+     name: 'tests',
+     message: 'Provide tests for your application. Then provide examples on how to run them. (Required)',
+     validate : 
+        testsInput => {
+            if (testsInput) {
+                return true;
+            } else {
+                console.log('You need to enter tests information!');
+                return false;
+            }
+        }  
+    },
+    {
+     type: 'checkbox',
+     name: 'license',
+     message: 'Select the license(s) you use for this project. You can selected multiple licenses if needed.',
+     choices: ['MIT', 'General Public License 2.0', 'General Public License 3.0', 'Mozilla Public License 2.0', 'BSD 3-Clause License', 'Apache License 2.0'], 
+    },
+    {
+     type: 'input',
+     name: 'github',
+     message: 'Enter your GitHub Username. (Required)',
+     validate : 
+        githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false;
+            }
+        }  
+    },
+    {
+     type: 'input',
+     name: 'emailAddress',
+     message: 'Enter your email address. (Required)',
+     validate : 
+        emailAddressInput => {
+            if (emailAddressInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address!');
+                return false;
+            }
+        }  
+    }
+    ])
+};
+
+questions()
+    .then(portfolioData => {
+        const pageMd = generateMarkdown(portfolioData);
+
+// TODO: Create a function to write README file
+fs.writeFile('README.md', pageMd, err => {
+    if (err) throw new Error(err);
+
+    console.log('Page created! Check out README.md in this directory to see it!');
+    });
+});
